254a92821d1a26b7195f4b67415f575900742f71
tchoforwilson <tchoforwilson@gmail.com>
2023-03-30 15:13:57 +0100

    User controller middlewares

---
+++ controllers/userController.js
@@ -0,0 +1,134 @@
+import multer from 'multer';
+import sharp from 'sharp';
+import AppError from '../utilities/appError.js';
+import catchAsync from '../utilities/catchAsync.js';
+import User from '../models/userModel.js';
+import factory from './handlerFactory.js';
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image file! Please upload only images', 400), true);
+  }
+};
+
+// Maximum size of user uploaded file(photo) 1MB
+const maxImageSize = 1 * 1000 * 1000;
+
+const upload = multer({
+  fileFilter: multerFilter,
+  limits: { fieldSize: maxImageSize },
+  storage: multerStorage,
+});
+
+const uploadUserPhoto = upload.single('photo');
+
+const resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/images/users/${req.file.filename}`);
+  next();
+});
+
+/**
+ * @breif Filter out unwanted fields in an object
+ * @param {Object} obj -> Provided object
+ * @param  {...any} allowedFields -> Allowed fields array
+ * @returns {Object}
+ */
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
+
+const getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
+const updateMe = catchAsync(async (req, res, next) => {
+  // 1) Send error if user posted password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword.'
+      ),
+      400
+    );
+  }
+
+  // 2) Filtered out unwanted fields names that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filteredBody.photo = req.file.filename;
+
+  // 3) Update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: updatedUser,
+  });
+});
+
+const deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, {
+    active: false,
+  });
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+const createUser = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not defined! Please use /signup instead',
+  });
+};
+
+const getUser = factory.getOne(User);
+const getAllUsers = factory.getAll(User);
+
+const updateUser = factory.updateOne(User);
+const deleteUser = catchAsync(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, { active: false });
+
+  if (!user) {
+    return next(new AppError('No user found with this ID', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+export default {
+  uploadUserPhoto,
+  resizeUserPhoto,
+  getMe,
+  updateMe,
+  deleteMe,
+  createUser,
+  getUser,
+  getAllUsers,
+  updateUser,
+  deleteUser,
+};
